70bdaf030ad6a21267beb0b307e2551b1475af27
unknown <cavendert@gmail.com>
2024-05-03 10:59:00 -0500

    bookreleases template complete

--- menu/src/pages/bookreleases.js
+++ menu/src/pages/bookreleases.js
@@ -7,15 +7,46 @@ import Seo from '../components/seo'
 const BookReleases = ({data}) => {
     return (
         <Layout>
-            <h1>Book Releases</h1>
+            <div>
+                <h1>Book Releases</h1>
+                {
+                    data.allContentfulRmBookReleases.edges.map(edge => (
+                        <div>
+                            <h2>{edge.node.title}</h2>
+                            <img src={edge.node.bookImage.file.url}/>
+                            <h2>{edge.node.author}</h2>
+                            <p>{edge.node.releaseDate}</p>
+                            <p>{edge.node.description.description}</p>
+                        </div>
+                    ))
+                }
+            </div>
+            
+
         </Layout>
     )
 }
 
 export default BookReleases
 
-/*export const query = graphql `
+export const query = graphql `
 {
-
+  allContentfulRmBookReleases(sort: {releaseDate: DESC}) {
+    edges {
+      node {
+        title
+        bookImage {
+          file {
+            url
+          }
+        }
+        author
+        releaseDate(formatString: "MM Do, YYYY")
+        description {
+          description
+        }
+      }
+    }
+  }
 }
-`*/
+`
